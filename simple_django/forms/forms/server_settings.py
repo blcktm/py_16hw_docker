ed6a11190e7fcb09afe01b5975decc8857e32118
ilyaprojec <7208899-ilyaprojec@users.noreply.gitlab.com>
2020-12-06 04:46:52 +0200

    edit DATABASES

--- simple_django/forms/forms/server_settings.py
+++ simple_django/forms/forms/server_settings.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_PASSWORD', ''),
+        'NAME': os.environ.get('POSTGRES_DB', ''),
         'USER': os.environ.get('POSTGRES_USER', ''),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
         'HOST': 'db',
